6fed7ab222fdde55f59d777940c9a12d4e399250
unknown <syafiqurohman7@gmail.com>
2024-03-13 17:15:27 +0700

    First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,116 @@
+/* eslint-disable global-require */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		//
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js',
+	],
+
+	darkMode: 'class',
+
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					50: "#f5f3ff",
+					100: "#ede7fe",
+					200: "#d6bcfa",
+					300: "#b794f4",
+					400: "#9f7aea",
+					500: "#805ad5",
+					600: "#6b46c1",
+					700: "#553c9a",
+					800: "#44337a",
+					900: "#322659"
+				},
+			},
+			fontFamily: {
+				sans: [
+					'Rubik',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'system-ui',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+				body: [
+					'Rubik',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'system-ui',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+				mono: [
+					'ui-monospace',
+					'SFMono-Regular',
+					'Menlo',
+					'Monaco',
+					'Consolas',
+					'Liberation Mono',
+					'Courier New',
+					'monospace',
+				],
+			},
+			transitionProperty: {
+				width: 'width',
+			},
+			textDecoration: ['active'],
+			minWidth: {
+				kanban: '28rem',
+			},
+		},
+	},
+
+	safelist: [
+		// In Markdown (README…)
+		'justify-evenly',
+		'overflow-hidden',
+		'rounded-md',
+
+		// From the Hugo Dashboard
+		'w-64',
+		'w-1/2',
+		'rounded-l-lg',
+		'rounded-r-lg',
+		'bg-gray-200',
+		'grid-cols-4',
+		'grid-cols-7',
+		'h-6',
+		'leading-6',
+		'h-9',
+		'leading-9',
+		'shadow-lg',
+		'bg-opacity-50',
+		'dark:bg-opacity-80',
+
+		// For Astro one
+		'grid',
+	],
+
+	plugins: [
+		//
+		require('flowbite/plugin'),
+		require('flowbite-typography'),
+		require('tailwind-scrollbar')({ nocompatible: true }),
+	],
+};
